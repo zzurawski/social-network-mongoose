e9c3aeb989d16a642c2a5c3962d451adf1349e71
zzurawski <116835382+zzurawski@users.noreply.github.com>
2023-04-16 19:37:27 -0400

    Add files via upload

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const {connect, connection} = require('mongoose');
+connect("mongodb://localhost:27017/socialDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
