5a1c5bd4b19d1b91d5f8598d71f3ad7351b2f8d9
zzurawski <116835382+zzurawski@users.noreply.github.com>
2023-04-16 19:39:21 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(routes);
+app.use(express.json());
+
+db.once('open', () => { app.listen(PORT, () => {console.log(`Server live: ${PORT}`)})})
